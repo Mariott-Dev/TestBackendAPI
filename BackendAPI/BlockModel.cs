15f63ea849624be025a6fb0fa04a1348b04a0da0
Dane Mariott <mariottdesign@gmail.com>
2023-06-08 15:08:11 -0600

    Add project files.

---
+++ BackendAPI/BlockModel.cs
@@ -0,0 +1,65 @@
+﻿namespace BackendAPI
+{
+    public class BlockModel
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class Argument
+        {
+            public string? controlType { get; set; }
+            public string? dataType { get; set; }
+            public string? description { get; set; }
+            public string? name { get; set; }
+        }
+
+        public class BlockDoc
+        {
+            public List<Argument>? arguments { get; set; }
+            public string? blockLibraryName { get; set; }
+            public List<Channel>? channels { get; set; }
+            public string? description { get; set; }
+            public List<object>? signals { get; set; }
+            public List<object>? slots { get; set; }
+            public List<Statistic>? statistics { get; set; }
+        }
+
+        public class Channel
+        {
+            public string? dataType { get; set; }
+            public string? description { get; set; }
+            public string? direction { get; set; }
+            public string? name { get; set; }
+        }
+
+        public class Header
+        {
+            public string? id { get; set; }
+            public string? name { get; set; }
+            public string? type { get; set; }
+        }
+
+        public class Parameters
+        {
+            public List<BlockDoc>? blockDocs { get; set; }
+        }
+
+        public class Response
+        {
+            public string? message { get; set; }
+            public bool? status { get; set; }
+        }
+
+        public class Root
+        {
+            public Header? header { get; set; }
+            public Parameters? parameters { get; set; }
+            public Response? response { get; set; }
+        }
+
+        public class Statistic
+        {
+            public string? dataType { get; set; }
+            public string? description { get; set; }
+            public string? name { get; set; }
+        }
+    }
+}
---
+++ BackendAPI/Controllers/BlockController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BackendAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BlockController : Controller
+    {
+        static byte[] GetLength(uint length)
+        {
+            return new byte[]
+            {
+                (byte)((length >> 24) & 0xFF),
+                (byte)((length >> 16) & 0xFF),
+                (byte)((length >> 8) & 0xFF),
+                (byte)(length & 0xFF)
+            };
+        }
+
+        [HttpGet]
+        [EnableCors]
+        public IActionResult Index()
+        {
+            // Use the provided JSON object to send
+            //string jsonMessage = @"{
+            //    ""header"": {
+            //        ""name"": ""GetAllBlockDocs"",
+            //        ""type"": ""command"",
+            //        ""id"": ""0""
+            //    }
+            //}";
+            JToken jAppSettings = JToken.Parse(
+                System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "appsettings.json"))
+            );
+            string jsonMessage = jAppSettings["JsonMessage"]!.ToString();  //will eventually need 'switch' for multiple commands
+
+            // Connect to the TCP server
+            string ipAddress = "127.0.0.1";
+            int port = 30000;
+
+            try
+            {
+                using (TcpClient client = new TcpClient(ipAddress, port))
+                using (NetworkStream stream = client.GetStream())
+                {
+                    // Prepare the message with the header and length
+                    byte[] header = Encoding.UTF8.GetBytes("BBSYNC");
+                    byte[] length = GetLength((uint)jsonMessage.Length);
+                    byte[] payload = Encoding.UTF8.GetBytes(jsonMessage);
+                    byte[] message = new byte[header.Length + length.Length + payload.Length];
+                    Buffer.BlockCopy(header, 0, message, 0, header.Length);
+                    Buffer.BlockCopy(length, 0, message, header.Length, length.Length);
+                    Buffer.BlockCopy(payload, 0, message, header.Length + length.Length, payload.Length);
+
+                    // Send the message
+                    stream.Write(message, 0, message.Length);
+                    Console.WriteLine("JSON message sent!");
+
+                    // Receive the response header and length
+                    byte[] responseHeader = new byte[6];
+                    byte[] responseLengthBytes = new byte[4];
+                    stream.Read(responseHeader, 0, responseHeader.Length);
+                    stream.Read(responseLengthBytes, 0, responseLengthBytes.Length);
+                    uint responseLength = (uint)((responseLengthBytes[0] << 24) | (responseLengthBytes[1] << 16) | (responseLengthBytes[2] << 8) | responseLengthBytes[3]);
+
+                    // Read the JSON payload based on the length
+                    byte[] responseData = new byte[responseLength];
+                    int bytesRead = 0;
+                    while (bytesRead < responseLength)
+                    {
+                        bytesRead += stream.Read(responseData, bytesRead, (int)responseLength - bytesRead);
+                    }
+
+                    string response = Encoding.UTF8.GetString(responseData);
+                    Console.WriteLine($"Received message: {response}");
+
+                    return Ok(response);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+
+                return StatusCode(500, e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e}");
+
+                return StatusCode(500, e);
+            }
+        }
+    }
+}
